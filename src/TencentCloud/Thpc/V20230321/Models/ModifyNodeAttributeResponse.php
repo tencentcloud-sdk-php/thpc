7fe021750fff5a053dc8ad7080baa14b8e5f12a0
tencentcloudapi <tencentcloudapi@tencent.com>
2026-01-22 04:16:08 +0800

    release 3.0.1526

--- src/TencentCloud/Thpc/V20230321/Models/ModifyNodeAttributeResponse.php
+++ src/TencentCloud/Thpc/V20230321/Models/ModifyNodeAttributeResponse.php
@@ -14,31 +14,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-namespace TencentCloud\Thpc\V20220401\Models;
+namespace TencentCloud\Thpc\V20230321\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * CreateCluster返回参数结构体
+ * ModifyNodeAttribute返回参数结构体
  *
- * @method string getClusterId() 获取<p>集群ID。</p>
- * @method void setClusterId(string $ClusterId) 设置<p>集群ID。</p>
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
-class CreateClusterResponse extends AbstractModel
+class ModifyNodeAttributeResponse extends AbstractModel
 {
-    /**
-     * @var string <p>集群ID。</p>
-     */
-    public $ClusterId;
-
     /**
      * @var string 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     public $RequestId;
 
     /**
-     * @param string $ClusterId <p>集群ID。</p>
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -54,10 +46,6 @@ class CreateClusterResponse extends AbstractModel
         if ($param === null) {
             return;
         }
-        if (array_key_exists("ClusterId",$param) and $param["ClusterId"] !== null) {
-            $this->ClusterId = $param["ClusterId"];
-        }
-
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
             $this->RequestId = $param["RequestId"];
         }
